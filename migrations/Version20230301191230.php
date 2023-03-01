f9bf23dce8b6c785d5774818f72dd1719adef0ba
Баранов Георгий <georgiy.baranov@infourok.biz>
2023-03-01 22:15:00 +0300

    init

--- migrations/Version20230301191230.php
+++ migrations/Version20230301191230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230301184011 extends AbstractMigration
+final class Version20230301191230 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20230301184011 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)');
+        $this->addSql('ALTER TABLE book ADD title VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE book');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE book DROP title');
     }
 }
